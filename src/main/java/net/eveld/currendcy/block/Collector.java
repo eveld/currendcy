0a1a0ef2e4872bd6b6b817f230ab7c914b2aa28e
Erik Veld <=>
2021-08-18 13:35:13 +0200

    Add ticking to blocks

--- src/main/java/net/eveld/currendcy/block/Collector.java
+++ src/main/java/net/eveld/currendcy/block/Collector.java
@@ -1,27 +1,33 @@
 package net.eveld.currendcy.block;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockRenderType;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.HorizontalFacingBlock;
-import net.minecraft.block.BlockEntityProvider;
+import net.minecraft.block.BlockWithEntity;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.BlockEntityTicker;
+import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.text.LiteralText;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
 import net.minecraft.state.StateManager;
+import net.minecraft.state.property.DirectionProperty;
 import net.minecraft.state.property.Properties;
 
+import net.eveld.currendcy.Currendcy;
 import net.eveld.currendcy.block.entity.CollectorEntity;
 
-public class Collector extends HorizontalFacingBlock implements BlockEntityProvider {
+public class Collector extends BlockWithEntity {
+  public static final DirectionProperty FACING = Properties.HORIZONTAL_FACING;
+
   public Collector(Settings settings) {
     super(settings);
-    setDefaultState(this.stateManager.getDefaultState().with(Properties.HORIZONTAL_FACING, Direction.NORTH));
+    setDefaultState(this.stateManager.getDefaultState());
   }
 
   @Override
@@ -35,11 +41,26 @@ public class Collector extends HorizontalFacingBlock implements BlockEntityProvi
   
   @Override
   public BlockEntity createBlockEntity(BlockPos pos, BlockState state) {
-      return new CollectorEntity(pos, state);
+    return new CollectorEntity(pos, state);
   }
 
   @Override
-	protected void appendProperties(StateManager.Builder<Block, BlockState> stateManager) {
-		stateManager.add(Properties.HORIZONTAL_FACING);
+  public BlockRenderType getRenderType(BlockState state) {
+    return BlockRenderType.MODEL;
+  }
+
+  @Override
+	protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+		builder.add(FACING);
 	}
+
+  @Override
+  public BlockState getPlacementState(ItemPlacementContext ctx) {
+    return getDefaultState().with(FACING, ctx.getPlayerFacing().getOpposite());
+  }
+
+  @Override
+  public <T extends BlockEntity> BlockEntityTicker<T> getTicker(World world, BlockState state, BlockEntityType<T> type) {
+    return world.isClient ? null : checkType(type, Currendcy.COLLECTOR_ENTITY, CollectorEntity::tick);
+  }
 }
